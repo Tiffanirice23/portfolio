e8403aab1ebd9cb5d9621a76b39982757fbcc6f2
Tiffani Rice <tiffanirice414@gmail.com>
2023-07-04 15:20:15 -0700

    JarJar Edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,7 +62,7 @@ class Header extends Component {
             <Switch 
               checked={this.state.checked}
               onChange={this.onThemeSwitchChange}
-              offColor="#baaa80"
+              offColor="#e8c2ca"
               onColor="#353535"
               className="react-switch mx-auto"
               width={90}
